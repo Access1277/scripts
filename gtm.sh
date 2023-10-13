473d0562f109f524386e8652915272ff7e2f86a6
Access1277 <108107724+Access1277@users.noreply.github.com>
2023-10-13 13:42:54 +0800

    Update gtm.sh

--- gtm.sh
+++ gtm.sh
@@ -1,78 +1,80 @@
 #!/bin/bash
-## Copyright ©UDPTeam
-## Script to keep-alive your DNSTT server domain record query from target resolver/local dns server
-## Run this script excluded to your VPN tunnel (split vpn tunneling mode)
-## run command: ./globe-civ3.sh l
+# Copyright © 2023 YourName
+# Script to keep-alive your DNSTT server domain record query from target resolver/local dns server
+# Run this script excluded from your VPN tunnel (split VPN tunneling mode)
+# Usage: $0 [loop|l]
 
-## Your DNSTT Nameserver & your Domain `A` Record
+# Your DNSTT Nameserver & your Domain `A` Record
 A='myudp.elcavlaw.com'
 NS='sdns.myudp.elcavlaw.com'
 A1='art1.bagito.tech'
 NS1='sdns.art1.bagito.tech'
 
-## Repeat dig cmd loop time (seconds) (positive integer only)
+# Repeat dig command loop time (seconds) (positive integer only)
 LOOP_DELAY=5
 
-## Add your DNS here
+# Add your DNS here
 declare -a HOSTS=('124.6.181.20')
 
-## Linux' dig command executable filepath
-## Select value: "CUSTOM|C" or "DEFAULT|D"
+# Linux' dig command executable filepath
+# Select value: "CUSTOM|C" or "DEFAULT|D"
 DIG_EXEC="DEFAULT"
-## if set to CUSTOM, enter your custom dig executable path here
+# If set to CUSTOM, enter your custom dig executable path here
 CUSTOM_DIG=/data/data/com.termux/files/home/go/bin/fastdig
 
-_VER=0.1
+VER=0.1
+
 case "${DIG_EXEC}" in
-  DEFAULT|D)
-  _DIG="$(command -v dig)"
-  ;;
-  CUSTOM|C)
-  _DIG="${CUSTOM_DIG}"
-  ;;
+ DEFAULT|D)
+ _DIG="$(command -v dig)"
+ ;;
+ CUSTOM|C)
+ _DIG="${CUSTOM_DIG}"
+ ;;
 esac
 
-if [ ! -x "$_DIG" ]; then
-  printf "Dig command failed to run, please install dig(dnsutils) or check \$DIG_EXEC & \$CUSTOM_DIG variable inside %s/%s file.\n" "$(cd -- "$(dirname "$0")" >/dev/null 2>&1; pwd -P)" "$(basename "$0")" && exit 1
+if [ ! "$_DIG" ]; then
+ printf "%b" "Dig command failed to run, " \
+ "please install dig (dnsutils) or check " \
+ "\$DIG_EXEC & \$CUSTOM_DIG variable inside $( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/$(basename "$0") file.\n" && exit 1
 fi
 
-check() {
-  for T in "${HOSTS[@]}"; do
-    for R in "$A" "$NS" "$A1" "$NS1"; do
-      if timeout -k 3 3 "$_DIG" "@$T" "$R" &> /dev/null; then
-        M=32
-      else
-        M=31
-      fi
-      echo -e "\e[1;${M}m\$ R:$R D:$T\e[0m"
-    done
-  done
-}
-
 endscript() {
-  unset A NS A1 NS1 LOOP_DELAY HOSTS _DIG DIG_EXEC CUSTOM_DIG T R M
-  exit 1
+ unset NS A LOOP_DELAY HOSTS _DIG DIG_EXEC CUSTOM_DIG T R M
+ exit 1
 }
 
 trap endscript 2 15
 
-echo "DNSTT Keep-Alive script <Lantin Nohanih>"
+check(){
+ for ((i=0; i<"${#HOSTS[*]}"; i++)); do
+  for R in "${A}" "${NS}" "${A1}" "${NS1}"; do
+   T="${HOSTS[$i]}"
+   [[ -z $(timeout -k 3 3 ${_DIG} "@${T}" "${R}") ]] && M=31 || M=32;
+   echo -e "\e[1;${M}m\$ R:${R} D:${T}\e[0m"
+   unset T R M
+  done
+ done
+}
+
+echo "DNSTT Keep-Alive script <YourName>"
 echo -e "DNS List: [\e[1;34m${HOSTS[*]}\e[0m]"
 echo "CTRL + C to close script"
 
-[[ "$LOOP_DELAY" -eq 1 ]] && ((LOOP_DELAY++))
+[[ "${LOOP_DELAY}" -eq 1 ]] && let "LOOP_DELAY++";
+
 case "${@}" in
-  loop|l)
-    echo "Script loop: ${LOOP_DELAY} seconds"
-    while true; do
-      check
-      echo '.--. .-.. . .- ... .     .-- .- .. -'
-      sleep "$LOOP_DELAY"
-    done
-    ;;
-  *)
-    check
-    ;;
+ loop|l)
+ echo "Script loop: ${LOOP_DELAY} seconds"
+ while true; do
+  check
+  echo '.--. .-.. . .- ... .     .-- .- .. -'
+  sleep ${LOOP_DELAY}
+ done
+ ;;
+ *)
+ check
+ ;;
 esac
 
 exit 0
