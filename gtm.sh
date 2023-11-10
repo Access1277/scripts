a379bec5f8533a0ac6fa34dbd79e293f80854bdb
Access1277 <108107724+Access1277@users.noreply.github.com>
2023-11-10 16:07:02 +0800

    Update gtm.sh

--- gtm.sh
+++ gtm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Your DNS resolver's IP addresses
-DNS_RESOLVERS=("112.198.115.44" "112.198.115.36" "124.6.181.20" "124.6.181.36")
+DNS_RESOLVERS=("127.0.0.1")
 
 # Domain records to query
-DOMAINS=("sdns.myudp.elcavlaw.com" "team-mamawers.elcavlaw.com")
+DOMAINS=("myudp.elcavlaw.com" "sdns.myudp.elcavlaw.com" "mamawers.elcavlaw.com" "team-mamawers.elcavlaw.com")
 
 # Set the number of parallel queries
-NUM_PARALLEL_QUERIES=4
+NUM_PARALLEL_QUERIES=2
 
 # Loop delay in seconds
 LOOP_DELAY=5
