bf398128b8d35f5977d54f53c2529432abb90744
Access1277 <108107724+Access1277@users.noreply.github.com>
2023-11-10 15:19:48 +0800

    Update gtm.sh

--- gtm.sh
+++ gtm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Copyright © 2023 YourName
-# DNS Keep-Alive script for DNSTT server domain record queries
+# DNS Keep-Alive script for DNSTT server NS record queries
 
-# DNSTT Nameservers and Domain `A` Records
+# DNSTT Nameservers and Domain `NS` Records
 DNS_SERVERS=('sdns.myudp.elcavlaw.com' 'team-mamawers.elcavlaw.com')
 DOMAINS=('myudp.elcavlaw.com' 'mamawers.elcavlaw.com')
 
@@ -10,30 +10,30 @@ DOMAINS=('myudp.elcavlaw.com' 'mamawers.elcavlaw.com')
 LOOP_DELAY=2
 
 # Add your DNS resolver IPs here
-RESOLVERS=('124.6.181.2' '124.6.181.36' '112.198.115.44' '112.198.115.36')
+RESOLVERS=('112.198.115.44' '112.198.115.36' '124.6.181.20' '124.6.181.36')
 
-# Function to perform DNS queries
-query_dns() {
+# Function to perform DNS NS record queries
+query_ns_records() {
   local resolver="$1"
   local domain="$2"
-  result=$(dig +short +stats +tries=1 +timeout=1 "@${resolver}" "${domain}" 2>/dev/null || echo 'Query failed')
-  echo "Resolver: ${resolver}, Domain: ${domain}, Result: ${result}"
+  result=$(dig +short +stats +tries=1 +timeout=1 NS "@${resolver}" "${domain}" 2>/dev/null || echo 'Query failed')
+  echo "Resolver: ${resolver}, Domain: ${domain}, NS Records: ${result}"
 }
 
 # Main loop
 while true; do
   for resolver in "${RESOLVERS[@]}"; do
     for domain in "${DOMAINS[@]}"; do
-      # Run queries in parallel for better speed
+      # Run NS record queries in parallel for better speed
       (
-        query_dns "${resolver}" "${domain}"
+        query_ns_records "${resolver}" "${domain}"
       ) &
     done
   done
-  
+
   # Wait for all parallel queries to complete
   wait
-  
+
   # Sleep before the next iteration
   sleep "${LOOP_DELAY}"
 done
