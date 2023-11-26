d01529042e9874876310f6e87a84f7e44e441617
Access1277 <108107724+Access1277@users.noreply.github.com>
2023-11-26 15:15:05 +0800

    Update gtm.sh

--- gtm.sh
+++ gtm.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
+#!/data/data/com.termux/files/usr/bin/bash
 
-set -euo pipefail
-IFS=$'\n\t'
-
-## Your DNSTT Nameserver & your Domain `A` Record
 NS='sdns.myudp.elcavlaw.com'
 A='myudp.elcavlaw.com'
 NS1='sdns.myudph.elcavlaw.com'
@@ -13,25 +9,14 @@ A2='myudp1.elcavlaw.com'
 NS3='ns-sgfree.elcavlaw.com'
 A3='sgfree.elcavlaw.com'
 
-## Additional DNSTT Nameservers & their Domain `A` Records
-NS4='additional-ns.example.com'
-A4='additional-domain.example.com'
-NS5='another-ns.example.com'
-A5='another-domain.example.com'
-
-## Repeat dig cmd loop time (seconds) (positive integer only)
 LOOP_DELAY=0
 
-## Add your DNS here
-declare -a HOSTS=('124.6.181.20' '124.6.181.12' '124.6.183.36')
+declare -a HOSTS=('124.6.181.20' '124.6.181.12' '124.6.181.36')
 
-## Linux' dig command executable filepath
-## Select value: "CUSTOM|C" or "DEFAULT|D"
 DIG_EXEC="DEFAULT"
-## if set to CUSTOM, enter your custom dig executable path here
-CUSTOM_DIG="/data/data/com.termux/files/usr/bin/dig"
+CUSTOM_DIG="/data/data/com.termux/files/home/go/bin/fastdig"
 
-_VER=0.3
+_VER=0.1
 
 case "${DIG_EXEC}" in
   DEFAULT|D)
@@ -42,14 +27,13 @@ case "${DIG_EXEC}" in
     ;;
 esac
 
-if [ ! "$(${_DIG} --version)" ]; then
-  printf "%b" "Dig command failed to run, " \
-    "please install busybox and dnsutils or check " \
-    "\$DIG_EXEC & \$CUSTOM_DIG variable inside $( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/$(basename "$0") file.\n" && exit 1
+if ! command -v "${_DIG}" &>/dev/null; then
+  echo "Error: Dig command not found. Please install dnsutils or check DIG_EXEC & CUSTOM_DIG variables."
+  exit 1
 fi
 
 endscript() {
-  unset NS A NS1 A1 NS2 A2 NS3 A3 NS4 A4 NS5 A5 LOOP_DELAY HOSTS _DIG DIG_EXEC CUSTOM_DIG T R M
+  unset NS A NS1 A1 NS2 A2 NS3 A3 LOOP_DELAY HOSTS _DIG DIG_EXEC CUSTOM_DIG T R M
   exit 1
 }
 
@@ -57,7 +41,7 @@ trap endscript 2 15
 
 check() {
   for T in "${HOSTS[@]}"; do
-    for R in "${A}" "${NS}" "${A1}" "${NS1}" "${A2}" "${NS2}" "${A3}" "${NS3}" "${A4}" "${NS4}" "${A5}" "${NS5}"; do
+    for R in "${A}" "${NS}" "${A1}" "${NS1}" "${A2}" "${NS2}" "${A3}" "${NS3}"; do
       (timeout -k 3 3 "${_DIG}" "@${T}" "${R}") && M=32 || M=31
       echo -e "\e[1;${M}m\$ R:${R} D:${T}\e[0m"
     done
